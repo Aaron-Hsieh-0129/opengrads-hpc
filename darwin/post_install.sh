0d78b78001117b1c15a737a6c161e6739f5a245d
Arlindo M. da Silva, Jr <dasilva@alum.mit.edu>
2019-02-10 09:17:15 -0500

    ams: on macOS pkg, make symlinks in /opt/local/bin instead of /usr/local/bin to avoid conflics with any possible COLA install

--- darwin/post_install.sh
+++ darwin/post_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-# Post install, creates symlinks in /usr/local/bin
+# Post install, creates symlinks in /opt/local/bin
 #
 
 binFiles="bufrscan geos grads grib2scan gribmap gribscan lats4d.sh ncep opengrads stnmap"
 
-/bin/mkdir -p /usr/local/bin
-cd /usr/local/bin
+/bin/mkdir -p /opt/local/bin
+cd /opt/local/bin
 
 for file in $binFiles; do
-    /bin/rm -rf /usr/local/bin/$file
+    /bin/rm -rf /opt/local/bin/$file
     /bin/ln -s /Applications/OpenGrADS/$file .
 done
 
